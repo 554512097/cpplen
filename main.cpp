64f1d76e15b828f546760d5b9e5f49520c6c2ca2
xiekun <Wcnm@,123456789>
2023-05-19 17:33:32 +0800

    23-2-19

--- main.cpp
+++ main.cpp
@@ -5,32 +5,12 @@
 #include "test_io/test_io.h"
 #include "strs/strs.h"
 #include <stdalign.h>
-
-typedef struct
-{
-    int value;
-    alignas(32) char alignedMemory[32];
-} cacheFriendly;
-
-typedef struct
-{
-    char a;
-    int c;
-    short b;
-    char d[14];
-} struct1;
+#include "utils/utils.h"
 
 using namespace std;
 
 int main(int argc, char const *argv[], char **envp)
 {
-    cout << "main start ..." << endl;
-
-    printf("sizeof(cacheFriendly): %d\n", sizeof(cacheFriendly));
-    printf("alignof(cacheFriendly): %d\n", alignof(cacheFriendly));
-
-    printf("struct1 : %d\n", sizeof(struct1));
-
     // test_args();
 
     // test_memory();
